38cb7a499764a06e0ff3227ba6b411acd06c99b9
DEVON DAVIAU <devon.daviau@yahoo.ca>
2017-01-25 15:46:03 -0500

    Add lesson03

---
+++ lesson03/callHello.js
@@ -0,0 +1,7 @@
+/**
+ * Created by devon on 2017-01-25.
+ */
+// link to the sayHello file
+let hello = require ('./sayHello');
+
+hello.sayHello();
---
+++ lesson03/sayHello.js
@@ -0,0 +1,10 @@
+/**
+ * Created by devon on 2017-01-25.
+ */
+// create a variable with a string value
+let hello = 'Hello World';
+
+// make this function public with the keyword "exports"
+exports.sayHello = function () {
+    console.log(hello);
+};
